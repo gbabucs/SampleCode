f9c30e05181c09710c5913e37db52b2176b3ec8d
tcs <tcs@Anusuya.local>
2012-12-07 17:28:24 +0530

    Initial Commit

---
+++ SampleCode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleCode
+//
+//  Created by tcs on 12/7/12.
+//  Copyright (c) 2012 tcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SampleCode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SampleCode
+//
+//  Created by tcs on 12/7/12.
+//  Copyright (c) 2012 tcs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
